1004fb22f2cbc18e96830d296d3c00e8d8a8805a
eunhye <keho0o11@gmail.com>
2024-05-10 15:32:04 +0900

    test

--- SualDaeSaeng/src/kr/or/ddit/admin/controller/admin.java
+++ SualDaeSaeng/src/kr/or/ddit/admin/controller/admin.java
@@ -7,10 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 public class admin extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println();
 	}
 	
 	

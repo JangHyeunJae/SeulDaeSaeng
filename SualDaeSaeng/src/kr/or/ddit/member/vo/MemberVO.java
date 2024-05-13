74256193b62593e97f4536e82d21eda4d6862810
PC-22 <PC-22@DESKTOP-MBFK0BS>
2024-05-13 09:42:16 +0900

    new freshfile

---
+++ SualDaeSaeng/src/kr/or/ddit/member/vo/MemberVO.java
@@ -0,0 +1,75 @@
+package kr.or.ddit.member.vo;
+
+import java.time.LocalDate;
+
+public class MemberVO {
+
+	private int userNo;
+	private String memName;
+	private String memNick;
+	private String memBirth;
+	private String memEmail;
+	private String memTel;
+	private String memClass;
+	private LocalDate memRegdt;
+	private int addrNo;
+	
+	
+	public int getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+	public String getMemName() {
+		return memName;
+	}
+	public void setMemName(String memName) {
+		this.memName = memName;
+	}
+	public String getMemNick() {
+		return memNick;
+	}
+	public void setMemNick(String memNick) {
+		this.memNick = memNick;
+	}
+	public String getMemBirth() {
+		return memBirth;
+	}
+	public void setMemBirth(String memBirth) {
+		this.memBirth = memBirth;
+	}
+	public String getMemEmail() {
+		return memEmail;
+	}
+	public void setMemEmail(String memEmail) {
+		this.memEmail = memEmail;
+	}
+	public String getMemTel() {
+		return memTel;
+	}
+	public void setMemTel(String memTel) {
+		this.memTel = memTel;
+	}
+	public String getMemClass() {
+		return memClass;
+	}
+	public void setMemClass(String memClass) {
+		this.memClass = memClass;
+	}
+	public LocalDate getMemRegdt() {
+		return memRegdt;
+	}
+	public void setMemRegdt(LocalDate memRegdt) {
+		this.memRegdt = memRegdt;
+	}
+	public int getAddrNo() {
+		return addrNo;
+	}
+	public void setAddrNo(int addrNo) {
+		this.addrNo = addrNo;
+	}
+
+
+
+}

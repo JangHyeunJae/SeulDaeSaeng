e9ac36c36c01f7dfdee6dfa2128db9c64640ab7a
PC-19 <PC-19@DESKTOP-NC79240>
2024-05-22 17:02:25 +0900

    .

--- SualDaeSaeng/src/kr/or/ddit/member/controller/LoginController.java
+++ SualDaeSaeng/src/kr/or/ddit/member/controller/LoginController.java
@@ -2,13 +2,11 @@ package kr.or.ddit.member.controller;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 import kr.or.ddit.member.service.IMemberService;
@@ -19,39 +17,41 @@ import kr.or.ddit.member.vo.UsersVO;
 @WebServlet("/login.do")
 public class LoginController extends HttpServlet {
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("/views/member/login.jsp").forward(req,resp);
-	}
+   private static final long serialVersionUID = 1L;
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.setCharacterEncoding("UTF-8");
-		String usersId = req.getParameter("id");
-		String usersPass = req.getParameter("pw");
-		Boolean isMemberLogin = Boolean.parseBoolean(req.getParameter("memberLogin"));
+   @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      req.getRequestDispatcher("/views/member/login.jsp").forward(req,resp);
+   }
 
-		IMemberService loginService = MemberServiceImpl.getInstance();
-		MemberVO memberVO = new MemberVO();
-		memberVO.setUsersId(usersId);
-		memberVO.setUsersPass(usersPass);
+   @Override
+   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      req.setCharacterEncoding("UTF-8");
+      String usersId = req.getParameter("id");
+      String usersPass = req.getParameter("pw");
+      Boolean isMemberLogin = Boolean.parseBoolean(req.getParameter("memberLogin"));
 
-		boolean isSuccess = loginService.loginCheck(memberVO, isMemberLogin);
+      IMemberService loginService = MemberServiceImpl.getInstance();
+      MemberVO memberVO = new MemberVO();
+      memberVO.setUsersId(usersId);
+      memberVO.setUsersPass(usersPass);
 
-		if (isSuccess) {
-			System.out.println("로그인성공");
-			MemberVO memDetail = loginService.getMemDetail(usersId);
-			UsersVO usersDetail = loginService.getUsersDetail(usersId);
-			req.getSession().setAttribute("usersId", usersId);
-			req.getSession().setAttribute("memDetail", memDetail);
-      req.getSession().setAttribute("usersPass", usersPass); 
-			req.getSession().setAttribute("usersRole", usersDetail.getUsersRole());
-			resp.sendRedirect(req.getContextPath() + "/main.do");
+      boolean isSuccess = loginService.loginCheck(memberVO, isMemberLogin);
 
-		} else {
-			JsonObject jsonObject = new JsonObject();
-			jsonObject.addProperty("isSuccess", "fail");
-			resp.sendRedirect("/views/member/login.jsp");
-		}
-	}
+      if (isSuccess) {
+         System.out.println("로그인성공");
+         MemberVO memDetail = loginService.getMemDetail(usersId);
+         UsersVO usersDetail = loginService.getUsersDetail(usersId);
+         req.getSession().setAttribute("usersId", usersId);
+         req.getSession().setAttribute("memDetail", memDetail);
+         req.getSession().setAttribute("usersPass", usersPass); 
+         req.getSession().setAttribute("usersRole", usersDetail.getUsersRole());
+         resp.sendRedirect(req.getContextPath() + "/main.do");
+
+      } else {
+         JsonObject jsonObject = new JsonObject();
+         jsonObject.addProperty("isSuccess", "fail");
+         resp.sendRedirect("/login.do");
+      }
+   }
 }
